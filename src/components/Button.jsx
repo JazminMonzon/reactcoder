c4ddcbfd1bb28382966f884af0101b076da16610
JazminMonzon <jazminmonzonarancibia@gmail.com>
2021-07-27 20:40:57 -0300

    Desafio 2

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button(props){
+    return(
+        <>
+        <button className="btn btn-light">{props.text}</button>
+        </>
+    )
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import Button from "./Button"
+
+export default function NavBar() {
+    return(
+        <>
+        <Button text = "Inicio" />
+        <Button text = "Discos" />
+        <Button text = "Tazas" />
+        <Button text = "Remeras" />
+        <Button text = "Contacto" />
+        </>
+    )
+}
